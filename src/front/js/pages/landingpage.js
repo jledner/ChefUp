7cc1372835fda300e0b9ea16f7b2bc50477fee0a
itrytocodethings <imwaynebailey@gmail.com>
2022-03-08 23:09:55 +0000

    getting started on landing page

--- src/front/js/pages/landingpage.js
+++ src/front/js/pages/landingpage.js
@@ -4,16 +4,32 @@ import { Link, useParams } from "react-router-dom";
 import { Context } from "../store/appContext";
 import rigoImageUrl from "../../img/rigo-baby.jpg";
 
-export const LandingPage = props => {
-
-
-    return (
-
-            <div>
-            <h1>This will be the landing page. 
-                Ideally, it displays differently if the user is logged in or not.<br></br>
-                
-                The tutorial can go on this page</h1>
+export const LandingPage = (props) => {
+  return (
+    <div>
+      <header className="landing-header mt-4">
+        <div className="user-info d-flex justify-content-between">
+          <div>
+            <h3>Hello, World!</h3>
+            <p>Ready to nom on some foods...</p>
+          </div>
+          <div>
+            <img
+              className="img-fluid"
+              src="https://forums.terraria.org/data/avatars/o/128/128493.jpg?1550988870"
+            ></img>
+          </div>
+        </div>
+      </header>
+      <div className="featured mt-3">
+        <div className="row">
+          <div className="col-12 col-xl-10 m-auto">
+            <div className="featured-main p-3">
+              <b>hello world</b>
             </div>
-    )
-}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
