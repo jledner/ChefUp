49683031f2f13d19784ea70da85960053e0a07b2
itrytocodethings <imwaynebailey@gmail.com>
2022-03-08 23:55:35 +0000

    fixed up featured

--- src/front/js/pages/landingpage.js
+++ src/front/js/pages/landingpage.js
@@ -23,9 +23,23 @@ export const LandingPage = (props) => {
       </header>
       <div className="featured mt-3">
         <div className="row">
-          <div className="col-12 col-xl-10 m-auto">
-            <div className="featured-main p-3">
-              <b>hello world</b>
+          <div className="col-12 col-xl-12 m-auto">
+            <div className="featured-main py-5 px-5 position-relative">
+              <div className="row">
+                <div className="col-12 col-xl-4">
+                  <h1>Featured Text</h1>
+                  <p>
+                    Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                    Dicta, ducimus voluptatum? Quod quae obcaecati, modi maxime
+                    quisquam maiores, voluptatem qui repudiandae incidunt
+                    consectetur ullam aliquam voluptatum. Soluta a unde
+                    voluptatum.
+                  </p>
+                  <span className="d-inline-block munch-later position-absolute">
+                    <i class="fas fa-teeth-open"></i>
+                  </span>
+                </div>
+              </div>
             </div>
           </div>
         </div>
