3559fe7e9354e3c41c08de56e6a2f3bcdbf17f0a
itrytocodethings <imwaynebailey@gmail.com>
2022-03-10 02:27:36 +0000

    reworked the landing page (still wip)

---
+++ src/front/js/component/FeaturedCard.js
@@ -0,0 +1,22 @@
+import propTypes from "prop-types";
+import React from "react";
+
+export const FeaturedCard = (props) => {
+  return (
+    <div className="featured d-flex justify-content-center">
+      <div class="card featured-card text-center">
+        <div class="card-body">
+          <img className="d-block" src={props.img} />
+          <h5 class="card-title">Featured</h5>
+          <p class="card-text">
+            With supporting text below as a natural lead-in to additional
+            content.
+          </p>
+          <a href="#" class="btn btn-primary">
+            Go somewhere
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
--- src/front/js/pages/landingpage.js
+++ src/front/js/pages/landingpage.js
@@ -3,47 +3,25 @@ import PropTypes from "prop-types";
 import { Link, useParams } from "react-router-dom";
 import { Context } from "../store/appContext";
 import rigoImageUrl from "../../img/rigo-baby.jpg";
+import foodImg from "../../img/foods/pls.jpg";
+import "../../styles/landing.css";
+import { FeaturedCard } from "../component/FeaturedCard";
 
 export const LandingPage = (props) => {
   return (
     <div>
-      <header className="landing-header mt-4">
-        <div className="user-info d-flex justify-content-between">
-          <div>
-            <h3>Hello, World!</h3>
-            <p>Ready to nom on some foods...</p>
-          </div>
-          <div>
-            <img
-              className="img-fluid"
-              src="https://forums.terraria.org/data/avatars/o/128/128493.jpg?1550988870"
-            ></img>
+      <header className="landing-header position-relative mt-2">
+        <div className="landing-bg d-flex justify-content-center align-items-center">
+          <div className="featured-text">
+            <h1>ChefUp</h1>
+            <p>
+              Cool fancy line here (styling isn't finished, don't panic guys -
+              Wayne)
+            </p>
           </div>
         </div>
+        <FeaturedCard img={foodImg} />
       </header>
-      <div className="featured mt-3">
-        <div className="row">
-          <div className="col-12 col-xl-12 m-auto">
-            <div className="featured-main py-5 px-5 position-relative">
-              <div className="row">
-                <div className="col-12 col-xl-4">
-                  <h1>Featured Text</h1>
-                  <p>
-                    Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-                    Dicta, ducimus voluptatum? Quod quae obcaecati, modi maxime
-                    quisquam maiores, voluptatem qui repudiandae incidunt
-                    consectetur ullam aliquam voluptatum. Soluta a unde
-                    voluptatum.
-                  </p>
-                  <button className="btn d-inline-block munch-later position-absolute">
-                    <i class="fas fa-teeth-open"></i>
-                  </button>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
     </div>
   );
 };
