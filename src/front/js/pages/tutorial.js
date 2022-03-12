cea6b59822f2a4da7443b815fd51dc4c070988b0
jledner <jledner7@gmail.com>
2022-03-12 19:08:57 +0000

    updates

--- src/front/js/pages/tutorial.js
+++ src/front/js/pages/tutorial.js
@@ -4,9 +4,13 @@ import { Card } from "../component/card";
 import { Link, useParams } from "react-router-dom";
 import { Context } from "../store/appContext";
 import rigoImageUrl from "../../img/rigo-baby.jpg";
+import { Demo } from "../pages/demo";
 import { FeaturedCard } from "../component/FeaturedCard";
 
 export const Tutorial = (props) => {
+  const { store, setStore } = useContext(Context);
+  let meals = store.meals;
+  console.log(meals);
   return (
     <div
       id="carouselExampleDark"
@@ -50,15 +54,22 @@ export const Tutorial = (props) => {
               </em>
             </h5>
             <div className="row row-cols-1 row-cols-md-2 g-4">
-              <Card image="https://i2.wp.com/www.downshiftology.com/wp-content/uploads/2020/04/Shrimp-Tacos-main-1.jpg" />
+              {meals.map((meal) => {
+                return <Card meal={meal} />;
+              })}
+              {/* <Card
+                image="https://www.jocooks.com/wp-content/uploads/2020/06/chicken-gyros-1-14.jpg"
+                name="Chicken Gyro"
+               />
               <div style={{ border: "5px solid green" }}>
-                <Card image="https://www.jocooks.com/wp-content/uploads/2020/06/chicken-gyros-1-14.jpg" />
+                <Card image="https://i2.wp.com/www.downshiftology.com/wp-content/uploads/2020/04/Shrimp-Tacos-main-1.jpg" />
               </div>
               <Card image="https://foodtasia.com/wp-content/uploads/2019/02/tuscan-pasta-20.jpg" />
-              <Card image="https://i.guim.co.uk/img/media/fdcd34886d1d9aa696cbac74d1a3293aa4808895/0_680_6355_3814/master/6355.jpg?width=1200&height=900&quality=85&auto=format&fit=crop&s=8b6d0cb3ca5aa21a21d485bef8f481ba" />
+              <Card image="https://i.guim.co.uk/img/media/fdcd34886d1d9aa696cbac74d1a3293aa4808895/0_680_6355_3814/master/6355.jpg?width=1200&height=900&quality=85&auto=format&fit=crop&s=8b6d0cb3ca5aa21a21d485bef8f481ba" /> */}
             </div>
           </div>
         </div>
+
         <div className="carousel-item" data-bs-interval="2000">
           <img
             src="https://img.freepik.com/free-photo/food-background-with-place-text-with-different-kinds-pasta-tomatoes-herbs-mushrooms-eggs-seasonings-scattered-light-marble-background-top-view-italian-cuisine-concept_90258-3592.jpg?size=626&ext=jpg"
@@ -66,36 +77,44 @@ export const Tutorial = (props) => {
             alt="..."
           />
           <div className="carousel-caption d-none d-md-block">
-            <h5>Second slide label</h5>
-            <p>Some representative placeholder content for the second slide.</p>
-            <div className="row row-cols-1 row-cols-md-1 g-4">
-              <Card
-                image="https://i2.wp.com/www.downshiftology.com/wp-content/uploads/2020/04/Shrimp-Tacos-main-1.jpg"
-                style={{
-                  minWidth: "40em",
-                  maxWidth: "40em",
-                  minHeight: "400em",
-                  maxHeight: "400em",
-                  objectFit: "cover",
-                  margin: "auto",
-                }}
-              />
+            <h5>Choose what's best for you!</h5>
+            <p>See prep time, calories, and cost for each item</p>
+            <FeaturedCard img="https://i2.wp.com/www.downshiftology.com/wp-content/uploads/2020/04/Shrimp-Tacos-main-1.jpg" />
+            <div style={{ background: "white", border: "4px solid black" }}>
+              <p className="card-text" style={{ margin: "0", padding: "0" }}>
+                Prep time: 20 minutes
+              </p>
+              <p className="card-text" style={{ margin: "0", padding: "0" }}>
+                Calories: 500
+              </p>
+              <p className="card-text" style={{ margin: "0", padding: "0" }}>
+                Price: $5.99
+              </p>
             </div>
           </div>
         </div>
-        <div className="carousel-item">
+
+        <div className="carousel-item" data-bs-interval="2000">
           <img
             src="https://img.freepik.com/free-photo/food-background-with-place-text-with-different-kinds-pasta-tomatoes-herbs-mushrooms-eggs-seasonings-scattered-light-marble-background-top-view-italian-cuisine-concept_90258-3592.jpg?size=626&ext=jpg"
             className="d-block w-100"
             alt="..."
           />
           <div className="carousel-caption d-none d-md-block">
-            <h5>Third slide label</h5>
-            <p>Some representative placeholder content for the third slide.</p>
-            <FeaturedCard img="https://i2.wp.com/www.downshiftology.com/wp-content/uploads/2020/04/Shrimp-Tacos-main-1.jpg" />
+            <h5>Grocery List</h5>
+            <p>
+              After you have selected all your meals, the ingredients will show
+              up here
+            </p>
+            <Demo />
+            <i>
+              We assume you have all household ingredients like butter, salt,
+              etc.
+            </i>
           </div>
         </div>
       </div>
+
       <button
         className="carousel-control-prev"
         type="button"
--- src/front/js/store/flux.js
+++ src/front/js/store/flux.js
@@ -40,6 +40,25 @@ const getState = ({ getStore, getActions, setStore }) => {
           name: "European",
         },
       ],
+
+      meals: [
+        {
+          name: "Shrimp Tacos",
+          img: "https://i2.wp.com/www.downshiftology.com/wp-content/uploads/2020/04/Shrimp-Tacos-main-1.jpg",
+        },
+        {
+          name: "Chicken Gyro",
+          img: "https://www.jocooks.com/wp-content/uploads/2020/06/chicken-gyros-1-14.jpg",
+        },
+        {
+          name: "Tuscan Chicken Pasta",
+          img: "https://foodtasia.com/wp-content/uploads/2019/02/tuscan-pasta-20.jpg",
+        },
+        {
+          name: "Flank Steak Sandwich",
+          img: "https://i.guim.co.uk/img/media/fdcd34886d1d9aa696cbac74d1a3293aa4808895/0_680_6355_3814/master/6355.jpg?width=1200&height=900&quality=85&auto=format&fit=crop&s=8b6d0cb3ca5aa21a21d485bef8f481ba",
+        },
+      ],
     },
     actions: {
       // Use getActions to call a function within a fuction
