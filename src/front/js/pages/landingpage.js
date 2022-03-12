64a311af5edb485cd2348649e53d3d98932a9259
itrytocodethings <imwaynebailey@gmail.com>
2022-03-12 00:53:00 +0000

    reworked landing header

--- src/front/js/pages/landingpage.js
+++ src/front/js/pages/landingpage.js
@@ -10,7 +10,7 @@ import { FeaturedCard } from "../component/FeaturedCard";
 export const LandingPage = (props) => {
   return (
     <div>
-      <header className="landing-header position-relative mt-2">
+      {/* <header className="landing-header position-relative mt-2">
         <div className="landing-bg d-flex justify-content-center align-items-center">
           <div className="featured-text">
             <h1>ChefUp</h1>
@@ -21,6 +21,42 @@ export const LandingPage = (props) => {
           </div>
         </div>
         <FeaturedCard img={foodImg} />
+      </header> */}
+      <header className="landing-header d-flex flex-column justify-content-center">
+        <div className="row">
+          <div className="col-12 col-xl-1"></div>
+          <div className="col-12 col-xl-6">
+            <h1>Search thousands of quick, delicous meals on Chefup!</h1>
+            <p>Nom nome nome nom...</p>
+            <form className="d-flex">
+              <div class="form-group">
+                <input
+                  type="text"
+                  class="form-control bg-transparent"
+                  id="exampleFormControlInput1"
+                  placeholder="name@example.com"
+                />
+              </div>
+              <div class="form-group">
+                <select
+                  class="form-control bg-transparent"
+                  id="exampleFormControlSelect1"
+                  placeholder="hi"
+                >
+                  <option>Categories</option>
+                  <option>1</option>
+                  <option>2</option>
+                  <option>3</option>
+                  <option>4</option>
+                  <option>5</option>
+                </select>
+              </div>
+              <button className="btn btn-header">
+                <i class="fas fa-search"></i>
+              </button>
+            </form>
+          </div>
+        </div>
       </header>
     </div>
   );
