13cd22936c47360292bb0f5cb6477d4d92507c2f
jeffreyally <jeffreyally@gmail.com>
2022-03-22 19:14:57 +0000

    working on random meal framework

--- src/front/js/pages/needrandommeals.js
+++ src/front/js/pages/needrandommeals.js
@@ -12,7 +12,31 @@ export const NeedRandomMeals = (props) => {
 
     return (
         <>
-        <h1>this page will show the user random meals as suggestions</h1>
+            <h1>this page will show the user random meals as suggestions. I need to test the API - Jeff 3/22</h1>
+
+
+            <form onSubmit={(e)=>{
+                e.preventDefault()
+                console.log('submit works')
+            }}>
+
+                <div class="form-check">
+                    <input class="form-check-input" type="checkbox" value="" id="vegetarian" onChange={(e) => {
+                        console.log(e.target.checked)
+                        console.log(e.target.id)
+                    }} />
+                    <label class="form-check-label" for="vegetarian">
+                        Vegetarian
+                    </label>
+                </div>
+
+                <button type="submit" class="btn btn-primary"> Submit</button>
+            </form>
+
+
+
         </>
+
+
     )
 }
