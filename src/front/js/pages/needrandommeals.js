cb094e74a5176b8ad4da77bea182c067f7b228cf
jeffreyally <jeffreyally@gmail.com>
2022-03-27 00:27:59 +0000

    working on random meals

--- src/front/js/pages/needrandommeals.js
+++ src/front/js/pages/needrandommeals.js
@@ -7,37 +7,8 @@ import rigoImageUrl from "../../img/rigo-baby.jpg";
 import { Demo } from "./demo";
 import { FeaturedCard } from "../component/FeaturedCard";
 
-//dummy meal prefs to test randomMeal
-/* 
- Jeff, i made a dummy mealPrefs object. I assume we would have something like this structure in our backend/db. 
-*/
-const mealPrefs = {
-  diet: ["vegetarian"],
-  intolerances: ["Dairy", "Shellfish"],
-};
-//function to generate the prefs string. it will looks something like '&diet=vegan,paleo&intolerances=dairy,something' with the above data.
-let generateMealPrefString = (prefs) => {
-  let prefReq = "";
-  //iterate over each key in the mealPrefs and store the key name in the string varialbe as '&example='
-  for (let key in prefs) {
-    let string = `&${key}=`;
-    //for each selection ex: vegan or paleo add those to the string so now '&example=diet1,diet2...'
-    prefs[key].forEach((selection, i) => {
-      //this determines if we should add a comma between selections. if the list/array of prefs[key] isn't the last item and is greater than one item.
-      string +=
-        i != prefs[key].length - 1 && prefs[key].length > 1
-          ? `${selection},`
-          : `${selection}`;
-    });
-    prefReq += string;
-  }
-  return prefReq;
-};
-let prefString = generateMealPrefString(mealPrefs);
 
-console.log(prefString);
-let RandomMealsUrl = `https://api.spoonacular.com/recipes/complexSearch?&maxReadyTime=20${prefString}&addRecipeInformation=true&ignorePantry=true&instructionsRequired=true&fillIngredients=true&addRecipeNutrition=true&apiKey=63c77d2857624c45a6a65b2ec5df33e0&number=100`;
-console.log(RandomMealsUrl);
+
 //   "https://api.spoonacular.com/recipes/random?limitLicense=true&tags=${strForURL}&number=20&apiKey=abb3fdf4028b4f0d989e7ee0b2b23b67";
 
 let strForURL = ""; //empty string that is populated with parameters to pass to the URL
@@ -49,7 +20,7 @@ export const NeedRandomMeals = (props) => {
   const { store, actions } = useContext(Context);
   const history = useHistory();
   let UrlTagsHandler = (e) => {
-    //I believe each checkbox that we create will receive this function. When a box is checked in the
+     //I believe each checkbox that we create will receive this function. When a box is checked in the
     //JSX this function adds that tag to the URLTags list. If unchecked, this function
     //removes that tag from the URLTags list.
     e.target.checked
@@ -60,22 +31,35 @@ export const NeedRandomMeals = (props) => {
   let  SubmitCheckboxForm = async (e) => {
     e.preventDefault();
     console.log("submit works");
-    console.log(`https://api.spoonacular.com/recipes/complexSearch?&maxReadyTime=20${prefString}&addRecipeInformation=true&ignorePantry=true&instructionsRequired=true&fillIngredients=true&addRecipeNutrition=true&apiKey=63c77d2857624c45a6a65b2ec5df33e0&number=100`)
-    await actions.getMeals(
-      `https://api.spoonacular.com/recipes/complexSearch?&maxReadyTime=20${prefString}&addRecipeInformation=true&ignorePantry=true&instructionsRequired=true&fillIngredients=true&addRecipeNutrition=true&apiKey=63c77d2857624c45a6a65b2ec5df33e0&number=100`,
-      'preftest'
-    );
-    history.push(`/meals/browse/preftest`);
-    //On Submit,the loop below goes through the list in URLTags and adds each string to the variable
-    //strForURL. If it reaches the end of the list, it adds the string without a comma at the end but
-    //otherwise it is adding a string with a comma until it reaches the end.
-    //e.g... dessert,vegetarian,vegan
-    // for (let i = 0; i < UrlTags.length; i++) {
-    //   i == UrlTags.length - 1
-    //     ? (strForURL = strForURL + UrlTags[i])
-    //     : (strForURL = strForURL + UrlTags[i] + ",");
-    // }
-    console.log("strForURL is", strForURL);
+    let diets = ['vegetarian', 'Vegan', 'Dairy Free', 'Gluten Free']
+    let intolerances = ['Peanut', 'Soy', 'Sulfite', 'Egg']
+    let URLforIntolerances = 'intolerances='
+    let URLforDiets = 'diet='
+    for(let checkedboxes of UrlTags){
+      
+      for(let items of intolerances){
+        if(checkedboxes==items){
+          URLforIntolerances += checkedboxes+','
+        }
+      }
+    }
+    for(let checkedboxes of UrlTags){
+      
+      for(let items of diets){
+        if(checkedboxes==items){
+          URLforDiets += checkedboxes+','
+        }
+      }
+    }
+    // console.log(URLforIntolerances)
+    // let result = URLforIntolerances.substring(0,URLforIntolerances.length-1);
+    // console.log(result)
+
+    console.log(URLforDiets)
+    let result = URLforDiets.substring(0,URLforDiets.length-1);
+    console.log(result)
+    
+  
     //below is a sample of the URL that is being logged
     // console.log(
     //   `https://api.spoonacular.com/recipes/random?limitLicense=true&tags=${prefString}&number=20&apiKey=abb3fdf4028b4f0d989e7ee0b2b23b67`
@@ -107,9 +91,9 @@ export const NeedRandomMeals = (props) => {
   console.log(strForURL);
   return (
     <>
-      <h1>this page will show the user random meals as suggestions.</h1>
+      <h1> example = '&diet=vegan,paleo&intolerances=dairy,something'</h1>
       <h3>
-        Feel free to open up the console log as you test this. - Jeff 3/23/22
+        Diets
       </h3>
 
       <form
@@ -119,33 +103,133 @@ export const NeedRandomMeals = (props) => {
       >
         <div class="form-check">
           <input
+          name="diet"
             class="form-check-input"
             type="checkbox"
             value=""
-            id="vegetarian"
+            id="Vegetarian"
             onChange={(e) => {
               console.log(e.target.id);
               UrlTagsHandler(e);
             }}
           />
-          <label class="form-check-label" for="vegetarian">
+          <label class="form-check-label" for="Vegetarian">
             Vegetarian
           </label>
         </div>
-
         <div class="form-check">
           <input
+          name="diet"
             class="form-check-input"
             type="checkbox"
             value=""
-            id="dessert"
+            id="Vegan"
             onChange={(e) => {
               console.log(e.target.id);
               UrlTagsHandler(e);
             }}
           />
-          <label class="form-check-label" for="dessert">
-            Dessert
+          <label class="form-check-label" for="Vegan">
+            Vegan
+          </label>
+        </div>
+        <div class="form-check">
+          <input
+          name="diet"
+            class="form-check-input"
+            type="checkbox"
+            value=""
+            id="Dairy Free"
+            onChange={(e) => {
+              console.log(e.target.id);
+              UrlTagsHandler(e);
+            }}
+          />
+          <label class="form-check-label" for="Dairy Free">
+            Dairy Free
+          </label>
+        </div>
+        <div class="form-check">
+          <input
+          name="diet"
+            class="form-check-input"
+            type="checkbox"
+            value=""
+            id="Gluten Free"
+            onChange={(e) => {
+              console.log(e.target.id);
+              UrlTagsHandler(e);
+            }}
+          />
+          <label class="form-check-label" for="Gluten Free">
+            Gluten Free
+          </label>
+        </div>
+        <h3>
+        Intolerances
+      </h3>
+        <div class="form-check">
+          <input
+          name="diet"
+            class="form-check-input"
+            type="checkbox"
+            value=""
+            id="Egg"
+            onChange={(e) => {
+              console.log(e.target.id);
+              UrlTagsHandler(e);
+            }}
+          />
+          <label class="form-check-label" for="Egg">
+            Egg
+          </label>
+        </div>
+        <div class="form-check">
+          <input
+          name="diet"
+            class="form-check-input"
+            type="checkbox"
+            value=""
+            id="Sulfite"
+            onChange={(e) => {
+              console.log(e.target.id);
+              UrlTagsHandler(e);
+            }}
+          />
+          <label class="form-check-label" for="Sulfite">
+            Sulfite
+          </label>
+        </div>
+        <div class="form-check">
+          <input
+          name="diet"
+            class="form-check-input"
+            type="checkbox"
+            value=""
+            id="Soy"
+            onChange={(e) => {
+              console.log(e.target.id);
+              UrlTagsHandler(e);
+            }}
+          />
+          <label class="form-check-label" for="Soy">
+            Soy
+          </label>
+        </div>
+        <div class="form-check">
+          <input
+          name="diet"
+            class="form-check-input"
+            type="checkbox"
+            value=""
+            id="Peanut"
+            onChange={(e) => {
+              console.log(e.target.id);
+              UrlTagsHandler(e);
+            }}
+          />
+          <label class="form-check-label" for="Peanut">
+            Peanut 
           </label>
         </div>
 
@@ -170,9 +254,7 @@ export const NeedRandomMeals = (props) => {
     </>
   );
 };
-let gg = `{trending.map((meal) => {
-  return <Card meal={meal} />;
-})}`
+
 
 let ogcode = `  <ol class="list-group list-group-numbered">
 {resultsfromfetch ? (
@@ -183,3 +265,50 @@ let ogcode = `  <ol class="list-group list-group-numbered">
   <div>resultsfromfetch variable is empty right now</div>
 )}
 </ol>`
+
+//Waynes code below
+
+//dummy meal prefs to test randomMeal
+/* 
+ Jeff, i made a dummy mealPrefs object. I assume we would have something like this structure in our backend/db. 
+*/
+// const mealPrefs = {
+//   diet: ["vegetarian"],
+//   intolerances: ["Dairy", "Shellfish"],
+// };
+// //function to generate the prefs string. it will looks something like '&diet=vegan,paleo&intolerances=dairy,something' with the above data.
+// let generateMealPrefString = (prefs) => {
+//   let prefReq = "";
+//   //iterate over each key in the mealPrefs and store the key name in the string varialbe as '&example='
+//   for (let key in prefs) {
+//     let string = `&${key}=`;
+//     //for each selection ex: vegan or paleo add those to the string so now '&example=diet1,diet2...'
+//     prefs[key].forEach((selection, i) => {
+//       //this determines if we should add a comma between selections. if the list/array of prefs[key] isn't the last item and is greater than one item.
+//       string +=
+//         i != prefs[key].length - 1 && prefs[key].length > 1
+//           ? `${selection},`
+//           : `${selection}`;
+//     });
+//     prefReq += string;
+//   }
+//   return prefReq;
+// };
+// let prefString = generateMealPrefString(mealPrefs);
+
+// console.log(prefString);
+
+//let RandomMealsUrl = `https://api.spoonacular.com/recipes/complexSearch?&maxReadyTime=20${prefString}&addRecipeInformation=true&ignorePantry=true&instructionsRequired=true&fillIngredients=true&addRecipeNutrition=true&apiKey=63c77d2857624c45a6a65b2ec5df33e0&number=100`;
+
+
+
+
+
+//takes valid url and takes us to browse page with results....watch the prefString
+
+// console.log(`https://api.spoonacular.com/recipes/complexSearch?&maxReadyTime=20${prefString}&addRecipeInformation=true&ignorePantry=true&instructionsRequired=true&fillIngredients=true&addRecipeNutrition=true&apiKey=63c77d2857624c45a6a65b2ec5df33e0&number=100`)
+//     await actions.getMeals(
+//       `https://api.spoonacular.com/recipes/complexSearch?&maxReadyTime=20${prefString}&addRecipeInformation=true&ignorePantry=true&instructionsRequired=true&fillIngredients=true&addRecipeNutrition=true&apiKey=63c77d2857624c45a6a65b2ec5df33e0&number=100`,
+//       'preftest'
+//     );
+//     history.push(`/meals/browse/preftest`);
