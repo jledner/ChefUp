0f675f590feee42ed3828f32cf7d6c5c8c7cc549
jeffreyally <jeffreyally@gmail.com>
2022-03-30 17:30:18 +0000

    minor touch ups to navbar

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -2,26 +2,27 @@ import React, { useContext } from "react";
 import { Link } from "react-router-dom";
 import { Context } from "../store/appContext";
 import { Card } from "./card";
+
 import { CartCard } from "./cartcard";
 export const Navbar = () => {
   const { store, actions } = useContext(Context);
   console.log(store);
   return (
-    <nav className="navbar navbar-light bg-light">
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+
+
+
+
       <div className="container">
         <Link to="/">
-          <span className="navbar-brand mb-0 h1">React Boilerplate</span>
+          <span className="navbar-brand mb-0 h1">Home(logo here)</span>
         </Link>
         <div className="ml-auto">
           <Link to="/tutorial">
             <button className="btn btn-primary">How To</button>
           </Link>
         </div>
-        <div className="ml-auto">
-          <Link to="/">
-            <button className="btn btn-primary">Home</button>
-          </Link>
-        </div>
+        
         <div className="ml-auto">
           <Link to="/signup">
             <button className="btn btn-primary">Sign Up</button>
@@ -48,6 +49,30 @@ export const Navbar = () => {
           <Link to="/profilepage/user">
             <button className="btn btn-primary">Profile</button>
           </Link>
+        </div>
+
+        {/* Note to self: It's data-bs-toggle="dropdown" not data-toggle="dropdown". */}
+        <div class="dropdown">
+
+          <button class="btn btn-info dropdown-toggle" type="button" id="dropdownMenu2" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            Sample "Menu?"
+          </button>
+          <div class="dropdown-menu" aria-labelledby="dropdownMenu2">
+            <Link to="/tutorial">
+            <button class="dropdown-item" type="button">How To</button>
+            </Link>
+            <Link to="/grocerylist">
+              <button class="dropdown-item" type="button">Grocery List</button>
+            </Link>
+            <Link to="/randommeals">
+
+              <button class="dropdown-item" type="button"> To Random Meals Page(in progress)</button>
+
+            </Link>
+            <Link to="/profilepage/user">
+              <button class="dropdown-item" type="button"> Profile</button>
+            </Link>
+          </div>
         </div>
 
         <button
@@ -107,5 +132,7 @@ export const Navbar = () => {
         </div>
       </div>
     </nav>
+
+
   );
 };
