dd1c7666542a014380695f5ea7a08b0f87dc1b0f
itrytocodethings <imwaynebailey@gmail.com>
2022-04-06 22:14:18 +0000

    added white

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -8,139 +8,169 @@ export const Navbar = () => {
   const { store, actions } = useContext(Context);
   console.log(store);
   return (
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-      <div className="container-fluid">
-        <Link to="/">
-          <span className="navbar-brand mb-0 h1">Home(logo here)</span>
-        </Link>
-        <div className="ml-auto">
-          <Link to="/tutorial">
-            <button className="btn btn-primary">How To</button>
-          </Link>
-        </div>
-
-        <div className="ml-auto">
-          <Link to="/signup">
-            <button className="btn btn-primary">Sign Up</button>
-          </Link>
-        </div>
-        <div className="ml-auto">
-          <Link to="/login">
-            <button className="btn btn-primary">Login</button>
-          </Link>
-        </div>
-        <div className="ml-auto">
-          <Link to="/grocerylist">
-            <button className="btn btn-primary">Grocery List</button>
-          </Link>
-        </div>
-        <div className="ml-auto">
-          <Link to="/randommeals">
-            <button className="btn btn-primary">
-              To Random Meals Page(in progress)
-            </button>
-          </Link>
-        </div>
-        <div className="ml-auto">
-          <Link to={store.user ? `/profilepage/${store.user.username}` : ""}>
-            <button className="btn btn-primary">Profile</button>
-          </Link>
-        </div>
-
-        {/* Note to self: It's data-bs-toggle="dropdown" not data-toggle="dropdown". */}
-        <div class="dropdown">
-          <button
-            class="btn btn-info dropdown-toggle"
-            type="button"
-            id="dropdownMenu2"
-            data-bs-toggle="dropdown"
-            aria-haspopup="true"
-            aria-expanded="false"
-          >
-            Sample "Menu?"
-          </button>
-          <div class="dropdown-menu" aria-labelledby="dropdownMenu2">
-            <Link to="/tutorial">
-              <button class="dropdown-item" type="button">
-                How To
-              </button>
-            </Link>
-            <Link to="/grocerylist">
-              <button class="dropdown-item" type="button">
-                Grocery List
-              </button>
-            </Link>
-            <Link to="/randommeals">
-              <button class="dropdown-item" type="button">
-                {" "}
-                To Random Meals Page(in progress)
-              </button>
-            </Link>
-            <Link to="/profilepage/user">
-              <button class="dropdown-item" type="button">
-                {" "}
+    <nav class="navbar navbar-expand-lg navbar-light py-3">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="#">
+          Navbar
+        </a>
+        <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
+          <ul class="navbar-nav mb-2 mb-lg-0 ms-auto">
+            <li class="nav-item">
+              <Link to="/" className="nav-link active">
+                Home
+              </Link>
+            </li>
+            <li class="nav-item">
+              <Link to="/login" className="nav-link active">
+                Login
+              </Link>
+            </li>
+            <li class="nav-item">
+              <Link to="/tutorial" className="nav-link active">
+                How to
+              </Link>
+            </li>
+            <li class="nav-item">
+              <Link to="/randommeals" className="nav-link active">
+                Random
+              </Link>
+            </li>
+            <li class="nav-item">
+              <Link to="/profilepage/user" className="nav-link active">
                 Profile
-              </button>
-            </Link>
-          </div>
+              </Link>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">
+                Link
+              </a>
+            </li>
+          </ul>
         </div>
-
         <button
+          class="navbar-toggler"
           type="button"
-          className="btn btn-primary"
-          data-bs-toggle="modal"
-          data-bs-target="#staticBackdrop"
-          onClick={() => {
-            // console.log(MealsInCart);
-          }}
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarTogglerDemo02"
+          aria-controls="navbarTogglerDemo02"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
         >
-          {/* Testing Cart{"(" + MealsInCart.length + ")"} */}
-          <i class="fas fa-shopping-cart"></i>
+          <span class="navbar-toggler-icon"></span>
         </button>
-
-        <div
-          class="modal fade"
-          id="staticBackdrop"
-          data-bs-backdrop="static"
-          data-bs-keyboard="false"
-          tabIndex="-1"
-          aria-labelledby="staticBackdropLabel"
-          aria-hidden="true"
-        >
-          <div class="modal-dialog modal-xl">
-            <div class="modal-content text-black">
-              <div class="modal-header">
-                <h5 className="modal-title text-dark" id="staticBackdropLabel">
-                  Your Cart
-                </h5>
-                <button
-                  type="button"
-                  className="btn-close"
-                  data-bs-dismiss="modal"
-                  aria-label="Close"
-                ></button>
-              </div>
-              <div class="modal-body">
-                {/* <div class="row row-cols-3 gy-4"> */}
-                <div class="row gy-3">
-                  {store.cart.map((meal, index) => {
-                    return <CartCard meal={meal} />;
-                  })}
-                </div>
-              </div>
-              <div class="modal-footer">
-                <button
-                  type="button"
-                  class="btn btn-secondary"
-                  data-bs-dismiss="modal"
-                >
-                  Close
-                </button>
-              </div>
-            </div>
-          </div>
-        </div>
       </div>
     </nav>
   );
 };
+
+// {/* <nav class="navbar navbar-expand-lg navbar-light py-3">
+//       <div className="container-fluid">
+//         <Link to="/">
+//           <span className="navbar-brand mb-0 h1">Home(logo here)</span>
+//         </Link>
+//         <div className="d-flex">
+//           <div className="ml-auto">
+//             <Link to="/tutorial">How To</Link>
+//           </div>
+//           <div className="ml-auto">
+//             <Link to="/signup">Sign Up</Link>
+//           </div>
+//           <div className="ml-auto">
+//             <Link to="/login">Login</Link>
+//           </div>
+//           <div className="ml-auto">
+//             <Link to="/grocerylist">Grocery List</Link>
+//           </div>
+//           <div className="ml-auto">
+//             <Link to="/randommeals">To Random Meals Page(in progress)</Link>
+//           </div>
+//           <div className="ml-auto">
+//             <Link to={store.user ? `/profilepage/${store.user.username}` : ""}>
+//               Profile
+//             </Link>
+//             {/* Note to self: It's data-bs-toggle="dropdown" not data-toggle="dropdown". */}
+//             <div class="dropdown">
+//               <button
+//                 class="btn btn-info dropdown-toggle"
+//                 type="button"
+//                 id="dropdownMenu2"
+//                 data-bs-toggle="dropdown"
+//                 aria-haspopup="true"
+//                 aria-expanded="false"
+//               >
+//                 Sample "Menu?"
+//               </button>
+//               <div class="dropdown-menu" aria-labelledby="dropdownMenu2">
+//                 <Link to="/tutorial">
+//                   <button class="dropdown-item" type="button">
+//                     How To
+//                   </button>
+//                 </Link>
+//                 <Link to="/grocerylist">
+//                   <button class="dropdown-item" type="button">
+//                     Grocery List
+//                   </button>
+//                 </Link>
+//                 <Link to="/randommeals">
+//                   <button class="dropdown-item" type="button">
+//                     {" "}
+//                     To Random Meals Page(in progress)
+//                   </button>
+//                 </Link>
+//                 <Link to="/profilepage/user">
+//                   <button class="dropdown-item" type="button">
+//                     {" "}
+//                     Profile
+//                   </button>
+//                 </Link>
+//                 <div
+//                   class="modal fade"
+//                   id="staticBackdrop"
+//                   data-bs-backdrop="static"
+//                   data-bs-keyboard="false"
+//                   tabIndex="-1"
+//                   aria-labelledby="staticBackdropLabel"
+//                   aria-hidden="true"
+//                 >
+//                   <div class="modal-dialog modal-xl">
+//                     <div class="modal-content text-black">
+//                       <div class="modal-header">
+//                         <h5
+//                           className="modal-title text-dark"
+//                           id="staticBackdropLabel"
+//                         >
+//                           Your Cart
+//                         </h5>
+//                         <button
+//                           type="button"
+//                           className="btn-close"
+//                           data-bs-dismiss="modal"
+//                           aria-label="Close"
+//                         ></button>
+//                       </div>
+//                       <div class="modal-body">
+//                         {/* <div class="row row-cols-3 gy-4"> */}
+//                         <div class="row gy-3">
+//                           {store.cart.map((meal, index) => {
+//                             return <CartCard meal={meal} />;
+//                           })}
+//                         </div>
+//                       </div>
+//                       <div class="modal-footer">
+//                         <button
+//                           type="button"
+//                           class="btn btn-secondary"
+//                           data-bs-dismiss="modal"
+//                         >
+//                           Close
+//                         </button>
+//                       </div>
+//                     </div>
+//                   </div>
+//                 </div>
+//               </div>
+//             </div>
+//           </div>
+//         </div>
+//       </div>
+//     </nav> */}
--- src/front/js/pages/profilepage.js
+++ src/front/js/pages/profilepage.js
@@ -21,100 +21,101 @@ export const ProfilePage = (props) => {
     <div>
       {store.user ? (
         <div>
-          {" "}
           <MainHeader sizeClass={"landing-header-small"} />
-          <section className="main">
-            <div className="user-profile">
-              <div className="row">
-                <div className="col-4">
-                  <div className="profile-nav bg-dark p-3">
-                    <div className="profile-nav-head d-flex flex-wrap  align-content-center align-items-center mb-3">
-                      <div className="avatar me-3">
-                        <img src={avatar} alt="" className="img-responsive" />
+          <div className="container">
+            <section className="main">
+              <div className="user-profile">
+                <div className="row">
+                  <div className="col-4">
+                    <div className="profile-nav bg-dark p-3">
+                      <div className="profile-nav-head d-flex flex-wrap  align-content-center align-items-center mb-3">
+                        <div className="avatar me-3">
+                          <img src={avatar} alt="" className="img-responsive" />
+                        </div>
+                        <h2>Hi, {store.user.username}</h2>
                       </div>
-                      <h2>Hi, {store.user.username}</h2>
-                    </div>
-                    <div>
-                      {" "}
-                      <div
-                        class="nav flex-column nav-pills me-3"
-                        id="v-pills-tab"
-                        role="tablist"
-                        aria-orientation="vertical"
-                      >
-                        <button
-                          class="nav-link active"
-                          id="v-pills-home-tab"
-                          data-bs-toggle="pill"
-                          data-bs-target="#v-pills-home"
-                          type="button"
-                          role="tab"
-                          aria-controls="v-pills-home"
-                          aria-selected="true"
+                      <div>
+                        {" "}
+                        <div
+                          class="nav flex-column nav-pills me-3"
+                          id="v-pills-tab"
+                          role="tablist"
+                          aria-orientation="vertical"
                         >
-                          User Info
-                        </button>
-                        <button
-                          class="nav-link"
-                          id="v-pills-profile-tab"
-                          data-bs-toggle="pill"
-                          data-bs-target="#v-pills-profile"
-                          type="button"
-                          role="tab"
-                          aria-controls="v-pills-profile"
-                          aria-selected="false"
-                        >
-                          Favorites
-                        </button>
+                          <button
+                            class="nav-link active"
+                            id="v-pills-home-tab"
+                            data-bs-toggle="pill"
+                            data-bs-target="#v-pills-home"
+                            type="button"
+                            role="tab"
+                            aria-controls="v-pills-home"
+                            aria-selected="true"
+                          >
+                            User Info
+                          </button>
+                          <button
+                            class="nav-link"
+                            id="v-pills-profile-tab"
+                            data-bs-toggle="pill"
+                            data-bs-target="#v-pills-profile"
+                            type="button"
+                            role="tab"
+                            aria-controls="v-pills-profile"
+                            aria-selected="false"
+                          >
+                            Favorites
+                          </button>
+                        </div>
                       </div>
                     </div>
                   </div>
-                </div>
-                <div className="col-8 bg-dark">
-                  <div class="tab-content" id="v-pills-tabContent">
-                    <div
-                      class="tab-pane fade show active"
-                      id="v-pills-home"
-                      role="tabpanel"
-                      aria-labelledby="v-pills-home-tab"
-                    >
-                      User Info
-                    </div>
-                    <div
-                      class="tab-pane fade"
-                      id="v-pills-profile"
-                      role="tabpanel"
-                      aria-labelledby="v-pills-profile-tab"
-                    >
-                      <div className="row">
-                        {store.user.favorites.length > 0
-                          ? store.user.favorites.map((fav) => (
-                              <Card meal={fav} />
-                            ))
-                          : "You have no favorites"}
+                  <div className="col-8 bg-dark">
+                    <div class="tab-content" id="v-pills-tabContent">
+                      <div
+                        class="tab-pane fade show active"
+                        id="v-pills-home"
+                        role="tabpanel"
+                        aria-labelledby="v-pills-home-tab"
+                      >
+                        User Info
                       </div>
-                    </div>
-                    <div
-                      class="tab-pane fade"
-                      id="v-pills-messages"
-                      role="tabpanel"
-                      aria-labelledby="v-pills-messages-tab"
-                    >
-                      ...
-                    </div>
-                    <div
-                      class="tab-pane fade"
-                      id="v-pills-settings"
-                      role="tabpanel"
-                      aria-labelledby="v-pills-settings-tab"
-                    >
-                      ...
+                      <div
+                        class="tab-pane fade"
+                        id="v-pills-profile"
+                        role="tabpanel"
+                        aria-labelledby="v-pills-profile-tab"
+                      >
+                        <div className="row">
+                          {store.user.favorites.length > 0
+                            ? store.user.favorites.map((fav) => (
+                                <Card meal={fav} />
+                              ))
+                            : "You have no favorites"}
+                        </div>
+                      </div>
+                      <div
+                        class="tab-pane fade"
+                        id="v-pills-messages"
+                        role="tabpanel"
+                        aria-labelledby="v-pills-messages-tab"
+                      >
+                        ...
+                      </div>
+                      <div
+                        class="tab-pane fade"
+                        id="v-pills-settings"
+                        role="tabpanel"
+                        aria-labelledby="v-pills-settings-tab"
+                      >
+                        ...
+                      </div>
                     </div>
                   </div>
                 </div>
               </div>
-            </div>
-          </section>
+            </section>
+          </div>
         </div>
       ) : (
         <div>not logged in</div>
