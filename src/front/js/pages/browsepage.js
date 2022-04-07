e9dc7833e1316b68898d449c08959bd901a5037d
itrytocodethings <imwaynebailey@gmail.com>
2022-04-07 01:03:05 +0000

    added containers to fix pages

--- src/front/js/pages/browsepage.js
+++ src/front/js/pages/browsepage.js
@@ -13,61 +13,40 @@ import { MainHeader } from "../component/MainHeader";
 export const BrowsePage = (props) => {
   const { store } = useContext(Context);
   const [searchQuery, setSearchQuery] = useState("");
-  const [showMoreMeals1, setShowMoreMeals1] = useState(false)
-  const [showMoreMeals2, setShowMoreMeals2] = useState(false)
-
-
+  const [showMoreMeals1, setShowMoreMeals1] = useState(false);
+  const [showMoreMeals2, setShowMoreMeals2] = useState(false);
 
   return (
-    <div>
+    <div className="container">
       <MainHeader sizeClass={"landing-header-small"} />
       <section className="category px-5 py-5">
         <div className="category-content">
           <h2>Browse Meals</h2>
-          
+
           <div className="row row-cols-4 gy-5">
-
-
             {/* <div className="row"> */}
-            {store.mealResults && store.mealResults.length > 0 ?
-
-
-
-              store.mealResults.slice(0, Math.round(store.mealResults.length / 3)).map((meal) => {
-                return <Card meal={meal} />;
-
-              })
-
-
-
-
-              : (
-                <div>no results :(</div>
-              )}
-
-           
+            {store.mealResults && store.mealResults.length > 0 ? (
+              store.mealResults
+                .slice(0, Math.round(store.mealResults.length / 3))
+                .map((meal) => {
+                  return <Card meal={meal} />;
+                })
+            ) : (
+              <div>no results :(</div>
+            )}
           </div>
-          {store.mealResults && store.mealResults.length > 10 ?
-              <div class="d-grid justify-content-center mx-auto my-5">
-                <Link to="/meals/page2">
-                  <button type="button" class="btn btn-light">To Page 2</button>
-
-                </Link>
-
-
-              </div>
-              : null}
-
-
-
-
-
-
-
-
+          {store.mealResults && store.mealResults.length > 10 ? (
+            <div class="d-grid justify-content-center mx-auto my-5">
+              <Link to="/meals/page2">
+                <button type="button" class="btn btn-light">
+                  To Page 2
+                </button>
+              </Link>
+            </div>
+          ) : null}
         </div>
-      </section >
-    </div >
+      </section>
+    </div>
   );
 };
 
@@ -91,10 +70,9 @@ let previouscode = `return (
       </div>
     </section>
   </div>
-);`
-
+);`;
 
 let button = `<button type="button" class="btn btn-dark" onClick={(e) => {
   console.log('clicked', showMoreMeals)
   setShowMoreMeals(!showMoreMeals)
-}}>Primary</button>`
+}}>Primary</button>`;
--- src/front/js/pages/browsepage3.js
+++ src/front/js/pages/browsepage3.js
@@ -9,44 +9,45 @@ import { Card } from "../component/card";
 import { MainHeader } from "../component/MainHeader";
 
 export const BrowsePage3 = (props) => {
-    const { store } = useContext(Context);
-    const [searchQuery, setSearchQuery] = useState("");
-    const history = useHistory();
+  const { store } = useContext(Context);
+  const [searchQuery, setSearchQuery] = useState("");
+  const history = useHistory();
 
+  return (
+    <div className="container">
+      <MainHeader sizeClass={"landing-header-small"} />
+      <section className="category px-5 py-5">
+        <div className="category-content">
+          <h2>Browse Meals</h2>
+          <div className="row row-cols-4 gy-5">
+            {store.mealResults
+              .slice(
+                Math.round((store.mealResults.length / 3) * 2),
+                store.mealResults.length
+              )
+              .map((meal) => {
+                return <Card meal={meal} />;
+              })}
+          </div>
 
-    return (
-        <div>
-            <MainHeader sizeClass={"landing-header-small"} />
-            <section className="category px-5 py-5">
-                <div className="category-content">
-                    <h2>Browse Meals</h2>
-                    <div className="row row-cols-4 gy-5">
-                        {
-                            store.mealResults.slice(Math.round((store.mealResults.length / 3) * 2), store.mealResults.length).map((meal) => {
-                                return <Card meal={meal} />;
-                            })
-                            }
-                    </div>
-
-                    <div class="d-flex justify-content-around mx-auto my-5 gap-2">
-                        <div>
-                            <Link to="/meals/page2">
-                                <button type="button" class="btn btn-light">To Page 2</button>
-
-                            </Link>
-                        </div>
-                        <div>
-                            <Link to="/randommeals">
-                                <button type="button" class="btn btn-light">Run Another Search</button>
-
-                            </Link>
-                        </div>
-                    </div>
-
-
-
-                </div>
-            </section >
-        </div >
-    );
-}
+          <div class="d-flex justify-content-around mx-auto my-5 gap-2">
+            <div>
+              <Link to="/meals/page2">
+                <button type="button" class="btn btn-light">
+                  To Page 2
+                </button>
+              </Link>
+            </div>
+            <div>
+              <Link to="/randommeals">
+                <button type="button" class="btn btn-light">
+                  Run Another Search
+                </button>
+              </Link>
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+};
